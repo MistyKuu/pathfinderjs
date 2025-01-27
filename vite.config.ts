a09aa2b65e4ed56e3c789eabb0de709f499400f7
Szepczyn <mistyksu@gmail.com>
2025-01-27 20:20:48 +0100

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // Replace with your actual repository name
+  base: "/pathfinderjs/",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
-})
+});
